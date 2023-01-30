90baa98e5b10aa73b5c056e18dfb3834e76d55e4
Zayne <star1582@naver.com>
2023-01-30 22:25:13 +0900

    infer type

---
+++ infer.ts
@@ -0,0 +1,10 @@
+function zip(
+  x: number,
+  y: string,
+  z: boolean
+): { x: number; y: string; z: boolean } {
+  return { x, y, z };
+}
+
+type Params = Parameters<typeof zip>;
+type First = Params[0];
