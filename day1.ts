cf59ea9ad3acd4f092efddbf52844c3446bcbb8d
Zayne <star1582@naver.com>
2023-02-05 23:21:11 +0900

    practice

--- day1.ts
+++ day1.ts
@@ -1,20 +1,20 @@
-function add(x: number, y: number): number {
-  return x + y;
-}
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
 
-const add2: (x: number, y: number) => number = (x, y) => x + y;
+// const add2: (x: number, y: number) => number = (x, y) => x + y;
 
-type Add = (x: number, y: number) => number;
-const add3: Add = (x, y) => x + y;
+// type Add = (x: number, y: number) => number;
+// const add3: Add = (x, y) => x + y;
 
-interface Add2 {
-  (x: number, y: number): number;
-}
-const add4: Add2 = (x, y) => x + y;
+// interface Add2 {
+//   (x: number, y: number): number;
+// }
+// const add4: Add2 = (x, y) => x + y;
 
-const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
-const arr: string[] = ["123", "456"];
-// <number>은 제네릭
-const arr2: Array<number> = [123, 456];
-//튜플 : 타입과 수가 정해져있음
-const arr3: [number, number, string] = [123, 456, "hello"];
+// const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
+// const arr: string[] = ["123", "456"];
+// // <number>은 제네릭
+// const arr2: Array<number> = [123, 456];
+// //튜플 : 타입과 수가 정해져있음
+// const arr3: [number, number, string] = [123, 456, "hello"];
--- first.ts
+++ first.ts
@@ -1,5 +1,21 @@
-interface Obj {
-  [key: string]: number;
-}
+const tuple: [string, number] = ["1", 2];
 
-const a: Record<string, number> = { a: 3, b: 5, c: 7 };
+// tuple[2] = "3";
+tuple.push("3");
+
+// let arr: string[] = [];
+// let arr2: Array<string> = [];
+// function rest(a: string, ...args: string[]) {
+//   console.log(a, ...args); // 1, [ 2 , 3 ]
+// }
+
+// rest("1", "2", "3");
+
+// type World = "world" | "hell";
+
+// type Greeting = `hello ${World}`;
+// const a: Greeting = "hello hell";
+// const b: Greeting = "hello world";
+
+// const a: string = "hello";
+// const b: String = "hello"; // 대문자 St ring은 래퍼 타입으로 사용 X
