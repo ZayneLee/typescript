6121ad03fce80c49ae15b364b7d366b36402888c
Zayne <star1582@naver.com>
2023-02-09 22:49:53 +0900

    union 연습

---
+++ union.ts
@@ -0,0 +1,22 @@
+// type A = { a: string };
+// const a: A = { a: "hello" };
+
+// // 상속 및 구현이 필요할 경우 interface 사용
+// interface B {
+//   a: string;
+// }
+// const b: B = { a: "hello" };
+
+// function add(x: string | number, y: string | number): string | number {
+//   return x + y;
+// }
+// const result:string =add(1,2);
+// add(1,2)
+
+//string 이면서 number
+// type A = { hello: "world" } & { zayne: "lee" };
+// const a: A = { hello: "world", zayne: "lee" };
+
+type A = string | number;
+const a: A = 5;
+const b: A = "hello";
